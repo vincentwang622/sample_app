971c7ea311f2aebd3e9f037c860b144644fb960a
vincentwang622 <shuyu.wang.0622@gmail.com>
2019-01-29 21:11:02 -0500

    layout and routes finished

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def full_title(pages_title = '')
+        base_title = "Ruby on Rails Tutorial Sample App"
+        if pages_title.empty?
+            base_title
+        else
+            pages_title + "|" + base_title
+        end
+    end
 end
